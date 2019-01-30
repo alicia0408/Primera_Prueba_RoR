3e0bb2facda391ebcc8438c02a105a84728c7b3e
Alicia <alicent.08.02@gmail.com>
2019-01-30 17:31:05 -0300

    Se agrega la ruta complete,list,las vistas y metodos correspondientes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+    before_action :find_todo , only:[:show,:edit,:update,:destroy,:complete]
     
     def index
         @todos = Todo.all
@@ -10,27 +11,44 @@ class TodosController < ApplicationController
     end
     
     def create
-        @todo = Todo.new(todo_params)
+     @todo = Todo.new(todo_params)
     @todo.save
         redirect_to todos_path
     end
 
     def show
-      @todo =  Todo.find(params[:id])
-    end
+     end
 
     def edit
-        @todo =  Todo.find(params[:id])
- end
+    end
 
  def update
-    @todo =  Todo.find(params[:id])
     @todo.update(todo_params)
     redirect_to todos_path
-end
-    
-private
+    end
+
+
+    def destroy
+        @todo.destroy
+        redirect_to todos_path
+  end
+
+  def complete
+    @completed = true
+    @completed.save
+    redirect_to todos_path
+  end
+     
+  def list
+    @todos = Todos.all
+  end
+ private
     def todo_params
         params.require(:todo).permit(:description, :completed)
     end
+    
+    def find_todo
+        @todo =  Todo.find(params[:id])
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get 'todos/:id', to: 'todos#show' 
   get 'todos/:id/edit', to: 'todos#edit'
   patch 'todos/:id', to: 'todos#update'
-  delete ''
+  delete 'todos/:id' , to: 'todos#destroy'
+  get 'todos/:id/complete', to: 'todos#complete'
+  get 'todos/list'
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
